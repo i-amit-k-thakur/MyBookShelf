9dfe4b09e06bfd651a2f4de27012932cea174a08
Amit Thakur <i.m.amit.k.thakur@gmail.com>
2025-10-02 14:41:33 +0100

    First commit

---
+++ MyBookShelf/MyBookShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBookShelfApp.swift
+//  MyBookShelf
+//
+//  Created by Amit Thakur on 02/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyBookShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
